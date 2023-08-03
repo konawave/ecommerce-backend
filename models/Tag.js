a77578c3822b7051ef0e5346a8ea52701992c51f
konawave <nickkjeffs@gmail.com>
2023-08-03 12:35:17 -0400

    more progress on routes. Revisiting routes activities because my route-writing skills are abysmal

--- models/Tag.js
+++ models/Tag.js
@@ -7,21 +7,22 @@ class Tag extends Model {}
 Tag.init(
   {
     id: {
-       type: DataTypes.INTEGER,
-       allowNull: false,
-       primaryKey: true,
-       autoIncrement: true
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    tagName: {
-      type: DataTypes.STRING
-    }
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'tag',
+    modelName: "tag",
   }
 );
 
